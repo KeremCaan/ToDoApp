45722389b34a94fa9cd6a8e1d0e5b8551b56ddce
KeremCaan <keremcaan@hotmail.com>
2022-08-27 23:11:07 +0300

    Initial Commit

---
+++ odev7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odev7
+//
+//  Created by MacbookPro on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
